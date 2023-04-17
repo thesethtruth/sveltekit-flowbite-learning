ff643c95d9b4707b3f207437234df1e16ef9b92d
thesethtruth <seth.van.wieringen@witteveenbos.com>
2023-04-17 11:28:07 +0200

    initial commit

---
+++ src/lib/components/DropdownOptions.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let options = [];
+    export let title = "Dropdown";
+
+    import { Button, Chevron, Dropdown, DropdownItem } from "flowbite-svelte";
+</script>
+
+<Button><Chevron>{title}</Chevron></Button>
+<Dropdown>
+    {#each options as option}
+        <DropdownItem>{option}</DropdownItem>
+    {/each}
+</Dropdown>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import DropdownOptions from "$lib/components/DropdownOptions.svelte";
+</script>
+
+<div class="flex justify-center p-10">
+    <DropdownOptions
+        title={"options"}
+        options={["option 1", "option 2", "option 3"]}
+    />
+</div>
